4eb30c4ae2466d82c5daed27e1541d1511bd9847
Thiago Ramalho <thiago.nramalho@gmail.com>
2023-06-02 18:57:26 -0300

    add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClaudeApiModule } from './claude-api/claude-api.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+export const PUBLIC_FOLDER_NAME = 'public';
+export const PUBLIC_PATH = `./${PUBLIC_FOLDER_NAME}`;
+export const PUBLIC_URL = `/${PUBLIC_FOLDER_NAME}`;
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', PUBLIC_FOLDER_NAME),
+      serveRoot: PUBLIC_URL,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
